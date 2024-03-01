7d4f491fcf09691dca75f4e48100b8f3c2c6f3d6
Kei <71589461+keiwsh@users.noreply.github.com>
2024-03-01 15:59:52 +0100

    Did the exercises up until Step 50

--- script.js
+++ script.js
@@ -19,6 +19,46 @@ const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
 
-function goStore () {
-    console.log("Going to store.");
+function goTown() {
+    button1.innerText = "Go to store";
+    button2.innerText = "Go to cave";
+    button3.innerText = "Fight dragon";
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+  }
+
+function goStore() {
+        text.innerText = "You enter the store.";
+        button1.innerText = "Buy 10 health (10 gold)";
+        button2.innerText = "Buy weapon (30 gold)";
+        button3.innerText = "Go to town square";
+      
+        button1.onclick = buyHealth;
+        button2.onclick = buyWeapon;
+        button3.onclick = goTown;
 }
+
+function goCave() {
+    console.log("Going to cave.");
+}
+
+function fightDragon() {
+    console.log("Fighting dragon.");
+}
+
+function buyHealth() {
+
+}
+
+function buyWeapon() {
+
+}
+
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+
